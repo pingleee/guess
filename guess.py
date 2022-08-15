8c26ec820dc90f741c9c7cbca3d10ccb6c07043a
pingleee <v65224@gmail.com>
2022-08-15 16:46:55 +0900

    times

--- guess.py
+++ guess.py
@@ -1,11 +1,13 @@
 import random
 
 r = random.randint(1, 100)
+t = 1
 while True:
     num = input('Please guess the number: ')
     num = int(num)
     if r == num:
-        print('Finally, right!')
+        print('You guess', t ,'times. Finally, right!')
+        break
     else:
-        print('The answer is:', r)
-    break
+        print('You guess', t ,'times. Wrong!')
+    t = t + 1
